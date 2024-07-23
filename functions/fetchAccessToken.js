66685e4314b3c5168aa7fc4b1a7dcfacab7a44ae
Chris Hutchins <chris@chrishutchins.com>
2024-07-23 14:22:26 -0700

    Update fetchAccessToken.js

--- functions/fetchAccessToken.js
+++ functions/fetchAccessToken.js
@@ -5,6 +5,17 @@ exports.handler = async (event, context) => {
   const clientId = 'pt8PQIzv9ECrXxWtnKEByZhyWZjOcmuDGmZDyxswQM0n4ij83k2jkk-6LA9Ztrcr';
   const clientSecret = '2yKYc3-J8tvlgBkajXqUDk3o-3RfZEI_DhJ3H31QCgKfIZBX_g1HT00Rj_XOeLlp';
   const redirectUri = 'https://chrishutchins.github.io/patreon-test.html';
+  
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: ''
+    };
+  }
 
   try {
     const response = await axios.post('https://www.patreon.com/api/oauth2/token', new URLSearchParams({
@@ -20,13 +31,20 @@ exports.handler = async (event, context) => {
     });
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
       body: JSON.stringify({ error: error.message })
     };
   }
 };
-
