640eec16ad8432f90324dc9bbd4e5e98601d032e
Chris Hutchins <chris@chrishutchins.com>
2024-07-23 14:41:37 -0700

    Update fetchAccessToken.js

--- netlify/functions/fetchAccessToken.js
+++ netlify/functions/fetchAccessToken.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
 exports.handler = async (event, context) => {
-  const { code } = JSON.parse(event.body);
   const clientId = 'pt8PQIzv9ECrXxWtnKEByZhyWZjOcmuDGmZDyxswQM0n4ij83k2jkk-6LA9Ztrcr';
   const clientSecret = '2yKYc3-J8tvlgBkajXqUDk3o-3RfZEI_DhJ3H31QCgKfIZBX_g1HT00Rj_XOeLlp';
   const redirectUri = 'https://chrishutchins.github.io/patreon-test.html';
@@ -14,6 +13,20 @@ exports.handler = async (event, context) => {
         'Access-Control-Allow-Headers': 'Content-Type'
       },
       body: ''
+    };
+  }
+
+  let code;
+  try {
+    code = JSON.parse(event.body).code;
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: JSON.stringify({ error: 'Invalid request body' })
     };
   }
 
