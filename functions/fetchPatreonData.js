1dc2de04b6d492cdbd6f690873a4ff69298fef54
Chris Hutchins <chris@chrishutchins.com>
2024-07-23 14:22:38 -0700

    Update fetchPatreonData.js

--- functions/fetchPatreonData.js
+++ functions/fetchPatreonData.js
@@ -3,6 +3,17 @@ const axios = require('axios');
 exports.handler = async (event, context) => {
   const { accessToken } = JSON.parse(event.body);
 
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: ''
+    };
+  }
+
   try {
     const response = await axios.get('https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields%5Buser%5D=full_name,vanity&fields%5Bmember%5D=currently_entitled_amount_cents', {
       headers: {
@@ -11,11 +22,19 @@ exports.handler = async (event, context) => {
     });
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
       body: JSON.stringify({ error: error.message })
     };
   }
