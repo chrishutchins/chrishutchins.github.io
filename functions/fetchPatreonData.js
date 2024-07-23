8122ba5fe5bf02aa19cb6e0c88abcf38e352d42f
Chris Hutchins <chris@chrishutchins.com>
2024-07-23 14:10:19 -0700

    Update fetchPatreonData.js

--- functions/fetchPatreonData.js
+++ functions/fetchPatreonData.js
@@ -1,1 +1,22 @@
+const axios = require('axios');
 
+exports.handler = async (event, context) => {
+  const { accessToken } = JSON.parse(event.body);
+
+  try {
+    const response = await axios.get('https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields%5Buser%5D=full_name,vanity&fields%5Bmember%5D=currently_entitled_amount_cents', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
