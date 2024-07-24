d95d73c08c752bd57f3301d64b8a75620e0e639f
Chris Hutchins <chris@chrishutchins.com>
2024-07-23 22:42:51 -0700

    Update fetchAccessToken.js

--- netlify/functions/fetchAccessToken.js
+++ netlify/functions/fetchAccessToken.js
@@ -1,36 +1,14 @@
 const axios = require('axios');
 
 exports.handler = async (event, context) => {
-  const clientId = 'pt8PQIzv9ECrXxWtnKEByZhyWZjOcmuDGmZDyxswQM0n4ij83k2jkk-6LA9Ztrcr';
-  const clientSecret = '2yKYc3-J8tvlgBkajXqUDk3o-3RfZEI_DhJ3H31QCgKfIZBX_g1HT00Rj_XOeLlp';
-  const redirectUri = 'https://www.allthehacks.com/p/patreon-redirect/';
-
-  if (event.httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type'
-      },
-      body: ''
-    };
-  }
-
-  let code;
   try {
-    code = JSON.parse(event.body).code;
-  } catch (error) {
-    return {
-      statusCode: 400,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type'
-      },
-      body: JSON.stringify({ error: 'Invalid request body' })
-    };
-  }
+    const { code } = JSON.parse(event.body);
+    const clientId = 'pt8PQIzv9ECrXxWtnKEByZhyWZjOcmuDGmZDyxswQM0n4ij83k2jkk-6LA9Ztrcr';
+    const clientSecret = process.env.PATREON_CLIENT_SECRET; // Use environment variable
+    const redirectUri = 'https://www.allthehacks.com/p/patreon-redirect/';
 
-  try {
+    console.log('Code received:', code);
+
     const response = await axios.post('https://www.patreon.com/api/oauth2/token', new URLSearchParams({
       'grant_type': 'authorization_code',
       'code': code,
@@ -42,6 +20,9 @@ exports.handler = async (event, context) => {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
+
+    console.log('Access token response:', response.data);
+
     return {
       statusCode: 200,
       headers: {
@@ -51,13 +32,15 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(response.data)
     };
   } catch (error) {
+    console.error('Error fetching access token:', error.response ? error.response.data : error.message);
+
     return {
       statusCode: 500,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type'
       },
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: error.response ? error.response.data : error.message })
     };
   }
 };
